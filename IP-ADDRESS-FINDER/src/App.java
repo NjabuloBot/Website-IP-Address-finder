9c601b6ddd2dd7eb4e7f0863199fb777e43f64f0
Njabulo <nnshiwu@gmail.com>
2024-02-18 18:36:50 +0200

    Declared the IPFINDER class which extends swing JFRAME and implements an ActionListener.

--- IP-ADDRESS-FINDER/src/App.java
+++ IP-ADDRESS-FINDER/src/App.java
@@ -3,6 +3,10 @@ import java.awt.event.*;
 import java.net.*;
 
 public class App {
+
+    public class IPFINDER extends JFrame implements ActionListener{
+        
+    }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
