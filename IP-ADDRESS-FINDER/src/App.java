7a940b5db228ebcbb5713937df55a0aa7aad40b4
Njabulo <nnshiwu@gmail.com>
2024-02-18 19:07:19 +0200

    Done with Website IP Adress finder.

--- IP-ADDRESS-FINDER/src/App.java
+++ IP-ADDRESS-FINDER/src/App.java
@@ -2,12 +2,43 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.net.*;
 
-public class App {
+public class App extends JFrame implements ActionListener{
 
-    public class IPFINDER extends JFrame implements ActionListener{
-        
+    JLabel labez;
+    JTextField textfield;
+    JButton butt;
+
+    App(){
+        super("IP FINDER TOOL");
+        labez = new JLabel("Enter URL: ");
+        labez.setBounds(50,70,150,20);
+        textfield = new JTextField();
+        textfield.setBounds(50,100,200,20);
+
+        butt = new JButton("Find IP");
+        butt.setBounds(50,150,80,30);
+        butt.addActionListener(this);
+        add(labez);
+        add(textfield);
+        add(butt);
+        setSize(300,300);
+        setLayout(null);
+        setVisible(true);
     }
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public void actionPerformed(ActionEvent e){
+        String url = textfield.getText();
+        try {
+            InetAddress ia =InetAddress.getByName(url);
+            String ip = ia.getHostAddress();
+            JOptionPane.showMessageDialog(this,ip);
+        } catch (UnknownHostException e1) {
+            JOptionPane.showMessageDialog(this,e1.toString());
+            // TODO: handle exception
+        }
     }
-}
+
+    public static void main(String[] args){
+        new App();
+    } 
+}  
